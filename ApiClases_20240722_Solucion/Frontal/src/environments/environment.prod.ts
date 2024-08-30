1b1e9dba47e7f5cca06228050a1bf15a9180af4a
Obelisko <orodaxrome65@gmail.com>
2024-08-30 14:13:35 +0200

    Configurar variables de entorno front

--- ApiClases_20240722_Solucion/Frontal/src/environments/environment.prod.ts
+++ ApiClases_20240722_Solucion/Frontal/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backend-estadistica-g6afbvbagbe3d6gr.spaincentral-01.azurewebsites.net/api/usuarios'
+    apiUrl: 'cleancashcourierapi-fdg9f7d4chb4gshy.spaincentral-01.azurewebsites.net/api'
   };
