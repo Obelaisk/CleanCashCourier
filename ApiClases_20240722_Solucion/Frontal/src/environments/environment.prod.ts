b324ba85c8a083c8a3a24c8cac162367415aebc5
Sergio Melero <sergiomele97@gmail.com>
2024-09-03 19:44:26 +0200

    oopsie me deje el https://

--- ApiClases_20240722_Solucion/Frontal/src/environments/environment.prod.ts
+++ ApiClases_20240722_Solucion/Frontal/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'cleancashcourierapi-fdg9f7d4chb4gshy.spaincentral-01.azurewebsites.net/api'
+    apiUrl: 'https://cleancashcourierapi-fdg9f7d4chb4gshy.spaincentral-01.azurewebsites.net/api'
   };
