628ec92fc3a27d183c77511fa2baa95184d32dab
Obelaisk <obed.rodas01@gmail.com>
2024-09-09 13:22:58 +0200

    Aaaaaaaaaaaaaañadiendo signalr a front

--- ApiClases_20240722_Solucion/Frontal/src/environments/environment.ts
+++ ApiClases_20240722_Solucion/Frontal/src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   apiContactos: 'https://localhost:8080/api/Contactos',
   apiTransacciones: 'https://localhost:8080/api/Transacciones', //Ni se utiliza en un principio
   apiPaises: 'https://localhost:8080/api/Paises',
-
+  apiHub: 'https://localhost:7138/signalrhubnotificacion'
   };
